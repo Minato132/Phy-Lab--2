fac3951c62247e35b6d4c1ab07710486c5c05d8a
Steven Chen <chensteven945@gmail.com>
2023-03-27 20:15:12 -0400

    Got data in their respective bins

--- Data.py
+++ Data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import statistics as stats
+import matplotlib.pyplot as plt
 
 with open('senriched.txt') as file:
     sig = pd.read_csv(file, skiprows = 2, header = None)
@@ -11,10 +12,23 @@ with open('bgdata.txt') as file1:
 sig_totstart = sig.iat[14951, 2]
 bg_totstart = bg.iat[5962, 2]
 
-#creating bins of size 100 ns
+#creating bins of size 50 ns
 bins = np.arange(0, max(sig[0]), 10)
 
-bin = pd.cut(sig[0], bins = bins, include_lowest = True, labels = False)
-#bin_count = sig['bins'].value_counts(sort = False, retbins = True)
+bg_bin = pd.cut(bg[0], bins = bins, include_lowest = True)
+bg_binsize = bg_bin.value_counts(sort = False)
+bg_binsize = bg_binsize / bg_totstart
 
-print(bin)
+sig_bin = pd.cut(sig[0], bins = bins, include_lowest= True)
+sig_binsize = sig_bin.value_counts(sort = False)
+sig_binsize = sig_binsize / sig_totstart
+
+final_bins = sig_binsize - bg_binsize
+
+final_bins = final_bins[final_bins >= 0]
+
+x = np.arange(len(final_bins))
+
+plt.scatter(x, np.log(final_bins))
+plt.show()
+
